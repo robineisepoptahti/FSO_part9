d963ae651d2dd878cbc97e1b7a9ef009d864109b
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-07-23 12:08:11 +0300

    Divided into components

---
+++ 9.15/my-app-name/src/components/Content.tsx
@@ -0,0 +1,27 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+const Content = (props: ContentProps) => {
+  const { courseParts } = props;
+  return (
+    <div>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+    </div>
+  );
+};
+
+export default Content;
