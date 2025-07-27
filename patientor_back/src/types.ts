21707b25a172002538ca0bf787e8a617c4b27bb6
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-07-27 18:39:05 +0300

    Added 9.23

--- patientor_back/src/types.ts
+++ patientor_back/src/types.ts
@@ -3,8 +3,6 @@ export interface Diagnosis {
   name: string;
   latin?: string;
 }
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
 
 export enum Gender {
   Male = "male",
@@ -21,6 +19,52 @@ export interface Patient {
   occupation: string;
   entries: Entry[];
 }
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type NewPatient = Omit<Patient, "id" | "entries">;
 
